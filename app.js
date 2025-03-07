c02498d02bc9b6e9caf723a05426b6290e482d4c
Luiz Martins <83313241+GalaxySqrt@users.noreply.github.com>
2025-03-07 02:38:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 80;
 
 const requestHandler = (request, response) => {
-  response.end('Bem-vindo ao AWS CodePipeline com Node.js! VERSÃO 2.0!!!!!');
+  response.end('Bem-vindo ao AWS CodePipeline com Node.js! VERSÃO 2.0!!!');
 };
 
 const server = http.createServer(requestHandler);
