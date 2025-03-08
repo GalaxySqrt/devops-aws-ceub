adaf6d9a4ceb99f6337066984959af0061f80f76
Galaxy <galaxy.cosmosr7@gmail.com>
2025-03-07 21:36:13 -0300

    owasp2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 80;
 
 const requestHandler = (request, response) => {
-  response.end('Bem-vindo ao AWS CodePipeline com Node.js! Rodando OWASP :)');
+  response.end('Bem-vindo ao AWS CodePipeline com Node.js! Rodando OWASP :) ');
 };
 
 const server = http.createServer(requestHandler);
